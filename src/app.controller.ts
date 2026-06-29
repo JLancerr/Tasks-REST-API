9cd63bb644c3a7fc8ed6a303a1f62a09ad3be6b6
Lance Jay Padillo <lancejaypadillo@gmail.com>
2026-06-29 14:22:08 +0800

    Final changes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Redirect } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Redirect('/api', 302)
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getIntro(): string {
+    return this.appService.getIntro();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getIntro(): string {
+    return `Welcome to the Tasks API! <br> Go to <a href="/api">/api</a> to access the Swagger API documentation.`;
   }
 }
